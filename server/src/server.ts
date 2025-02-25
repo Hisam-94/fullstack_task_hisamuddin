bfd679a60a8dda190dc4f9cbc28a9352c0f2625c
Hisamuddin Shaikh <hisamuddin94@gmail.com>
2025-02-25 17:54:23 +0530

    Added url in cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,6 +16,7 @@ const allowedDomains = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:5000",  
+  "https://fullstack-task-hisamuddin-1.onrender.com",
 ];
 
 const corsOptions = {
